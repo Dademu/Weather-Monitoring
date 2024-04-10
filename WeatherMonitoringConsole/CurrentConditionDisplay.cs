d94df5327c56937af75389fd713ce4bffe6eb012
Dademu <a00278997@mycambrian.ca>
2024-04-10 14:02:46 -0400

    update

--- WeatherMonitoringConsole/CurrentConditionDisplay.cs
+++ WeatherMonitoringConsole/CurrentConditionDisplay.cs
@@ -4,52 +4,40 @@ namespace WeatherMonitoringConsole;
 
 public class CurrentConditionDisplay : IDisplay<string>
 {
-    
-    private double result;
-    private DateTime dateAndTime = new DateTime();
-    public  void Display(string param)
-    {
 
+     private double result;
+     private DateTime dateAndTime = new DateTime();
+     public void Display(string param)
+     {
 
-        if(double.TryParse(param, out result))
-        {
-            switch(result){
-            case <= -20.0: 
-                Console.WriteLine("{0}°C: Extreme Cold, ", result);
-                Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                break;
+          if (double.TryParse(param, out result))
+          {
+               switch (result)
+               {
+                    case <= -20.0:
+                         Console.WriteLine("{0}°C: Extreme Cold, ", result);
+                         break;
 
-            case <= -5.0: 
-                 Console.WriteLine("{0}°C: Cold", result);
-                 Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                 break;
-            case <= 10.0: 
-                 Console.WriteLine("{0}°C: Cool", result);
-                 Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                 break;
-            case <= 25.0: 
-                 Console.WriteLine("{0}°C: Mild", result);
-                 Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                 break;
-            case <=35.0: 
-                 Console.WriteLine("{0}°C: Walm", result);
-                 Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                 break;
-            case <=70.0: 
-                 Console.WriteLine("{0}°C: Extremely warm", result);
-                 Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                 break;
-            default:
-                Console.WriteLine("Date: {}", dateAndTime.Date);
-                Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
-                throw new Exception("wrong value inserted");
-             };
-        }
-    }
+                    case <= -5.0:
+                         Console.WriteLine("{0}°C: Cold", result);
+                         break;
+                    case <= 10.0:
+                         Console.WriteLine("{0}°C: Cool Day", result);
+                         break;
+                    case <= 25.0:
+                         Console.WriteLine("{0}°C: Mild", result);
+                         break;
+                    case <= 35.0:
+                         Console.WriteLine("{0}°C: Walm", result);
+                         break;
+                    case <= 70.0:
+                         Console.WriteLine("{0}°C: Hot Day", result);
+                         break;
+                    default:
+                         throw new Exception("wrong value inserted");
+               };
+               Console.WriteLine("Date: {}", dateAndTime.Date);
+               Console.WriteLine("Time: {}", dateAndTime.TimeOfDay);
+          }
+     }
 }
--- WeatherMonitoringConsole/WeatherStation.cs
+++ WeatherMonitoringConsole/WeatherStation.cs
@@ -4,16 +4,19 @@ using WeatherMonitoringConsole.interfaces;
 
 public class WeatherStation
 {
-    public static IDisplay<string> CreateDisplay(string request){
-        switch(request){
+    public static IDisplay<string> CreateDisplay(string request)
+    {
+        switch (request)
+        {
             case "CurrentConditionDisplay":
                 return new CurrentConditionDisplay();
-            
+
             case "StatisticsDisplay":
                 return new StatisticsDisplay();
             case "ForecastDisplay":
                 return new ForecastDisplay();
             default:
-                throw new ArgumentException("Please input one of the following options to proceed with the operation: CurrentConditionDisplay, StatisticsDisplay, ForecastDisplay");            }
-        }
+                throw new ArgumentException("Please input one of the following options to proceed with the operation: CurrentConditionDisplay, StatisticsDisplay, ForecastDisplay");
+        }
     }
+}
